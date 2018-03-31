f4721c14093b71c7ac4ad7a1cf962fbc8d142629
Michelle Strout <mstrout@cs.arizona.edu>
2018-03-31 16:09:19 -0700

    Stand in code does something but is a mess.

--- src/PA8Main.java
+++ src/PA8Main.java
@@ -11,8 +11,7 @@ import javafx.util.Duration;
 
 public class PA8Main extends Application {
 
-    private GraphicsContext gc;
-    private static double delay = 0;
+    private static double delay = 3;
 
     public static void main(String[] args) {
         launch(args);
@@ -29,9 +28,9 @@ public class PA8Main extends Application {
         // Note this is opposite of the Garden in PA6.
         //Canvas canvas = new Canvas(num_cols * plot_size * CELLSIZE,
         //        num_rows * plot_size * CELLSIZE);
-        int canvas_pixels_across = 45;
-        int canvas_pixels_down = 22;
-        Canvas canvas = new Canvas(pixels_across, pixels_down);
+        final int SIZE_ACROSS = 100;
+        final int SIZE_DOWN = 250;
+        Canvas canvas = new Canvas(SIZE_ACROSS, SIZE_DOWN);
 
         // Command TextArea will hold the commands from the file
         int text_pixels_down = 120;
@@ -45,21 +44,34 @@ public class PA8Main extends Application {
 
         // Store a reference to the canvas so can draw to it
         // in other methods.
-        gc = canvas.getGraphicsContext2D();
+        GraphicsContext gc = canvas.getGraphicsContext2D();
         primaryStage.setTitle("Garden");
         primaryStage.setScene(new Scene(p));
 
         // Call the draw method.
-        draw(gc);  // can we pass in gc?
+        final int RECT_SIZE = 20;
+        Color c = Color.valueOf("RED");
+        gc.setFill(c);
+        gc.fillRect(50, 50, RECT_SIZE, RECT_SIZE);
+        command.appendText("Hello World!\n");
+
+        // gardenDraw(gc);
 
         // Show everything on the stage
         primaryStage.show();
 
         //------- Placeholder code (REMOVE once you have the above code)
         // By this point you will need to have read the delay from the
-        // file.  Here delay is just set to a constant.
-        double delay = 3;
-        Integer count = 10;  // number of times placeholder code updates
+        // file.
+        // This count object is taking the place of the Scanner.
+        // Below you will want to pass a reference to your Scanner into
+        // your "read in the next command" code. The Scanner object
+        // keeps track of where it is in the file. For the placeholder
+        // code we are providing here, there is no Scanner. Instead
+        // there is this Counter object that the lambda function will
+        // have a reference to.
+        Counter countref = new Counter(10); // number of times placeholder code
+                                            // updates
 
         //==========
         // Update GUI based on value of delay(seconds to wait)
@@ -76,22 +88,27 @@ public class PA8Main extends Application {
             //==== Code that should be executed after each delay goes in here.
             // read in the next command
             // if there was a command left in the file:
-            //     apply that command to the garden
-            //     draw to the canvas
-            //     append the command to the text field, command.appendText(...);
-            //     call wait.playFromStart(); 
+            // * apply that command to the garden
+            // * draw to the canvas by calling your gardenDraw(gc)
+            // * append the command to the text field, command.appendText(...);
+            // * call wait.playFromStart();
             // else:
-            //     call wait.stop();
+            // * call wait.stop();
 
             //---- Placeholder code (REMOVE once you have the above code)
             // This place holder code will draw a rectangle that
             // moves down the screen count times.
-            if (count>0) {
-                int RECT_SIZE = 10;
-                Color c = Color.valueOf("BLUE");
+            if (countref.count > 0) {
+                // final int RECT_SIZE = 20;
+                // c = Color.valueOf("BLUE");
                 gc.setFill(c);
-                gc.fillRect(canvas_pixels_across/2, count*RECT_SIZE, RECT_SIZE);
-                command.appendText("Hello there, count="+count);
+                int x = SIZE_ACROSS / 2;
+                int y = countref.count * RECT_SIZE;
+                gc.fillRect(x, y, RECT_SIZE, RECT_SIZE);
+                command.appendText(
+                        "count=" + countref.count + ", x=" + x + ", y = " + y
+                                + "\n");
+                countref.count--;
                 wait.playFromStart();
             } else {
                 wait.stop();
